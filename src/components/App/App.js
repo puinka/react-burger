ba0411fd0562b64facf3fd62682b736b62556fff
Valeria <vozzheva@mail.ru>
2022-10-25 17:08:10 +0100

    feat add loader

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,6 +1,7 @@
 import styles from "./app.module.css";
 import { INGREDIENTS_URL } from "../../utils/constants.js";
 import { useEffect, useState } from "react";
+import BounceLoader from "react-spinners/BounceLoader";
 import AppHeader from "../AppHeader/AppHeader";
 import BurgerIngredients from "../BurgerIngredients/BurgerIngredients";
 import BurgerConstructor from "../BurgerConstructor/BurgerConstructor";
@@ -13,6 +14,7 @@ function App() {
     fetchData();
   }, []);
 
+  const [isLoading, setIsLoading] = useState(false);
   const [ingredients, setIngredients] = useState();
   const [isOrderDetailsOpen, setOrderDetailsOpen] = useState(false);
   const [isIngredientDetailsOpen, setIngredientDetailsOpen] = useState(false);
@@ -20,6 +22,7 @@ function App() {
 
   const fetchData = async () => {
     try {
+      setIsLoading(true);
       const res = await fetch(INGREDIENTS_URL);
       if (!res.ok) {
         const message = `Ошибка HTTP: ${res.status}`;
@@ -27,6 +30,7 @@ function App() {
       }
       const json = await res.json();
       setIngredients(json.data);
+      setIsLoading(false);
     } catch (err) {
       alert("Ошибка: " + err);
     }
@@ -47,30 +51,45 @@ function App() {
   };
 
   return (
-    <div className={styles.app}>
-      <AppHeader />
-      <main className={styles.main}>
-        {ingredients && (
-          <BurgerIngredients
-            data={ingredients}
-            handleIngredientClick={handleShowIngredientDetails}
-          />
-        )}
-        {ingredients && (
-          <BurgerConstructor data={ingredients} makeOrder={handleCreateOrder} />
-        )}
-      </main>
-      {isOrderDetailsOpen && (
-        <Modal onCloseClick={closeAllModals}>
-          <OrderDetails number="034536" />
-        </Modal>
+    <>
+      {isLoading ? (
+        <BounceLoader
+          className={styles.loader}
+          color="#4C4CFF"
+          size={80}
+          aria-label="Loading Spinner"
+          data-testid="loader"
+        />
+      ) : (
+        <div className={styles.app}>
+          <AppHeader />
+          <main className={styles.main}>
+            {ingredients && (
+              <BurgerIngredients
+                data={ingredients}
+                handleIngredientClick={handleShowIngredientDetails}
+              />
+            )}
+            {ingredients && (
+              <BurgerConstructor
+                data={ingredients}
+                makeOrder={handleCreateOrder}
+              />
+            )}
+          </main>
+          {isOrderDetailsOpen && (
+            <Modal onCloseClick={closeAllModals}>
+              <OrderDetails number="034536" />
+            </Modal>
+          )}
+          {isIngredientDetailsOpen && (
+            <Modal title="Детали ингредиента" onCloseClick={closeAllModals}>
+              <IngredientDetails item={currentIngredient} />
+            </Modal>
+          )}
+        </div>
       )}
-      {isIngredientDetailsOpen && (
-        <Modal title="Детали ингредиента" onCloseClick={closeAllModals}>
-          <IngredientDetails item={currentIngredient} />
-        </Modal>
-      )}
-    </div>
+    </>
   );
 }
 
