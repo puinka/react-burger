295a7a3feb3a7b83ef49ad14f2605f68a09f7d3b
Valeria <vozzheva@mail.ru>
2022-12-04 14:06:38 +0000

    feat: add notFound404 page

--- src/pages/NotFound404.jsx
+++ src/pages/NotFound404.jsx
@@ -1,3 +1,26 @@
+import { Button } from "@ya.praktikum/react-developer-burger-ui-components";
+import { useEffect, useRef } from "react";
+import styles from "./notFound.module.css";
+import lottie from "lottie-web";
+import { Link } from "react-router-dom";
+
 export default function NotFound404() {
-  return <div>NotFound404</div>;
+  const container = useRef(null);
+  useEffect(() => {
+    lottie.loadAnimation({
+      container: container.current,
+      render: "svg",
+      loop: true,
+      autoplay: true,
+      animationData: require("../images/not-found.json"),
+    });
+  }, []);
+  return (
+    <main className={styles.main}>
+      <div className={styles.container} ref={container}></div>
+      <Link to="/">
+        <Button>На главную</Button>
+      </Link>
+    </main>
+  );
 }
