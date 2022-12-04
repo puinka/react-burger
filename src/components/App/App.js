7ea10b205cc77889ef77c150adcece45584494f1
Valeria <vozzheva@mail.ru>
2022-12-04 12:25:48 +0000

    feat: add routs

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -5,11 +5,18 @@ import { getIngredients } from "../../services/actions/ingredients";
 import BounceLoader from "react-spinners/BounceLoader";
 import AppHeader from "../AppHeader/AppHeader";
 import HomePage from "../../pages/HomePage";
+import NotFound404 from "../../pages/NotFound404";
 import { resetIngredientModal } from "../../services/actions/currentIngredient";
 import Modal from "../Modal/Modal";
 import IngredientDetails from "../Modal/IngredientDetails/IngredientDetails";
 
 import { INGREDIENT_TYPES } from "../../utils/constants.js";
+import { Route, Switch } from "react-router-dom";
+import LoginPage from "../../pages/LoginPage";
+import RegisterPage from "../../pages/RegisterPage";
+import ForgotPasswordPage from "../../pages/ForgotPasswordPage";
+import ResetPasswordPage from "../../pages/ResetPasswordPage";
+import ProfilePage from "../../pages/ProfilePage";
 
 function App() {
   const dispatch = useDispatch();
@@ -38,7 +45,32 @@ function App() {
       ) : (
         <div className={styles.app}>
           <AppHeader />
-          <HomePage closeAllModals={closeAllModals} />
+          <Switch>
+            <Route exact path="/login">
+              <LoginPage />
+            </Route>
+            <Route exact path="/">
+              <HomePage closeAllModals={closeAllModals} />
+            </Route>
+            <Route exact path="/register">
+              <RegisterPage />
+            </Route>
+            <Route exact path="/forgot-password">
+              <ForgotPasswordPage />
+            </Route>
+            <Route exact path="/reset-password">
+              <ResetPasswordPage />
+            </Route>
+            <Route exact path="/profile">
+              <ProfilePage />
+            </Route>
+            <Route exact path="/ingredients/:id">
+              <IngredientDetails />
+            </Route>
+            <Route path="*">
+              <NotFound404 />
+            </Route>
+          </Switch>
         </div>
       )}
       {currentIngredient && (
---
+++ src/pages/NotFound404.jsx
@@ -0,0 +1,3 @@
+export default function NotFound404() {
+  return <div>NotFound404</div>;
+}
