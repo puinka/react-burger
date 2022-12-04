209deec6753803c5656a8007987e0a53e36ac2d1
Valeria <vozzheva@mail.ru>
2022-12-04 12:06:37 +0000

    ref: move home to a separate page

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,13 +1,10 @@
 import styles from "./app.module.css";
 import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { DndProvider } from "react-dnd";
-import { HTML5Backend } from "react-dnd-html5-backend";
 import { getIngredients } from "../../services/actions/ingredients";
 import BounceLoader from "react-spinners/BounceLoader";
 import AppHeader from "../AppHeader/AppHeader";
-import BurgerIngredients from "../BurgerIngredients/BurgerIngredients";
-import BurgerConstructor from "../BurgerConstructor/BurgerConstructor";
+import HomePage from "../../pages/HomePage";
 import { resetIngredientModal } from "../../services/actions/currentIngredient";
 import Modal from "../Modal/Modal";
 import IngredientDetails from "../Modal/IngredientDetails/IngredientDetails";
@@ -16,7 +13,7 @@ import { INGREDIENT_TYPES } from "../../utils/constants.js";
 
 function App() {
   const dispatch = useDispatch();
-  const { isLoading, ingredients } = useSelector((store) => store.ingredients);
+  const { isLoading } = useSelector((store) => store.ingredients);
   const { currentIngredient } = useSelector((store) => store.ingredientModal);
 
   useEffect(() => {
@@ -41,14 +38,7 @@ function App() {
       ) : (
         <div className={styles.app}>
           <AppHeader />
-          <DndProvider backend={HTML5Backend}>
-            <main className={styles.main}>
-              {ingredients && (
-                <BurgerIngredients closeAllModals={closeAllModals} />
-              )}
-              <BurgerConstructor />
-            </main>
-          </DndProvider>
+          <HomePage closeAllModals={closeAllModals} />
         </div>
       )}
       {currentIngredient && (
---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,20 @@
+import { DndProvider } from "react-dnd";
+import { HTML5Backend } from "react-dnd-html5-backend";
+import { useSelector } from "react-redux";
+import BurgerIngredients from "../components/BurgerIngredients/BurgerIngredients";
+import BurgerConstructor from "../components/BurgerConstructor/BurgerConstructor";
+import styles from "./home.module.css";
+
+const HomePage = ({ closeAllModals }) => {
+  const { ingredients } = useSelector((store) => store.ingredients);
+  return (
+    <DndProvider backend={HTML5Backend}>
+      <main className={styles.main}>
+        {ingredients && <BurgerIngredients closeAllModals={closeAllModals} />}
+        <BurgerConstructor />
+      </main>
+    </DndProvider>
+  );
+};
+
+export default HomePage;
