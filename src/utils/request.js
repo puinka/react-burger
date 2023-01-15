466aef3bb56af09d870804e08158425373eb63b9
Valeria <vozzheva@mail.ru>
2023-01-15 17:03:15 +0200

    add: install axios

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,7 +1,13 @@
+import axios from "axios";
+
 const handleServerResponse = (res) => {
   return res.ok ? res.json() : res.json().then((err) => Promise.reject(err));
 };
 
+// export const request = async (url, options) => {
+//   return await fetch(url, options).then((res) => handleServerResponse(res));
+// };
+
 export const request = async (url, options) => {
-  return await fetch(url, options).then((res) => handleServerResponse(res));
+  return await axios(url, options);
 };
